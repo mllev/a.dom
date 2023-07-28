b85234332ef1968c28e9a131ef832783484e35fc
Matt Levenstein <m@pop-os.localdomain>
2023-07-27 22:26:57 -0400

    new cli

--- cli.js
+++ cli.js
@@ -1,99 +1,191 @@
 #! /usr/bin/env node
 
-let adom = require('./index')
-let fs = require('fs')
-let path = require('path')
-let config = {}
-let dir = process.cwd()
-let routes = {};
-let help = `
+const fs = require('fs');
+const path = require('path');
+const dir = process.cwd();
+const config = {};
+
+const help = `
 usage: adom [options]
   options:
-    create <name> Create a project boilerplate
-    dev           start a development server that statically serves a directory
-    -p <name>     the public directory for the development server
-    -r <route>    a route so the development server knows how to map adom files to urls
-                  example: adom dev -r /=index.adom -r /home=home.adom
-`
+    create <name> --ssg Create a project boilerplate with an example build script for a statically generated site
+                  --ssr Create a project boilerplate for a server rendered or hybrid site
+`;
 
-/***
- 
-  adom create --spa --backend
-  adom create --mpa --static
-  adom create --mpa --dynamic
-  
-  
-  SPA
-  
-  adom.app({
-    publicDir: './public',
-    routes: {
-      '*': {
-        path: 'src/index.adom'
-      }
-    }
+const ssgBuild = `const adom = require('adom-js');
+
+const posts = [
+ { id: 'post1', content: 'Blog post 1' },
+ { id: 'post2', content: 'Blog post 2' }
+];
+
+const build = async () => {
+  adom.compile({
+    input: 'src/index.adom',
+    output: 'public/index.html,
+    minify: true,
   });
 
-  MPA
+  await Promise.all(posts.map(async (post) => {
+    await adom.compile({
+      input: 'src/blog.adom',
+      output: \`public/blog/\${post.id}.html\`,
+      minify: true,
+      data: { post: post.content }
+    });
+    console.log(\`Compiled \${post.id}.html\`);
+  }));
+  console.log('Completed build');
+};
 
-  adom.app({
-    publicDir: './public',
-    routes: {
-      '/': {
-        path: 'src/home.adom'
-      },
-      '/about': {
-        path: 'src/about.adom'
-      },
-      '/blog/:post_id': {
-        path: 'src/blog.adom',
-        data: async () => {
-          return { post: 'Blog post 1' }
-        }
+function exec() {
+  if (process.argv.includes('dev')) {
+    await build()
+    adom.serve({ publicDir: './public' });
+  } else {
+    await build();
+  }
+}
+
+exec();
+`;
+
+const ssrBuild = `const adom = require('adom-js');
+
+const prod = !process.argv.includes('dev');
+
+const posts = {
+  post1: 'Blog post 1',
+  post2: 'Blog post 2'
+};
+
+adom.serve({
+  publicDir: './public',
+  cache: prod,
+  minify: prod,
+  routes: {
+    '/': {
+      path: 'src/index.adom'
+    },
+    '/blog/:post_id': {
+      path: 'src/blog.adom',
+      data: async (req) => {
+        return { post: posts[req.params.post_id] }
       }
     }
-  });
+  }
+});
+`;
 
-  SSG
+const ssgPackageFile = (name) => `{
+  "name": "${name}",
+  "version": "0.0.1",
+  "description": "",
+  "main": "buidl.js",
+  "scripts": {
+    "prod": "node build",
+    "dev": "node build dev"
+  },
+  "keywords": [],
+  "author": "",
+  "license": "ISC",
+  "dependencies": {
+    "adom-js": "^0.19.13"
+  }
+}
+`;
 
-  adom.compile
+const ssrPackageFile = (name) => `{
+  "name": "${name}",
+  "version": "0.0.1",
+  "description": "",
+  "main": "server.js",
+  "scripts": {
+    "prod": "node server",
+    "dev": "node server dev"
+  },
+  "keywords": [],
+  "author": "",
+  "license": "ISC",
+  "dependencies": {
+    "adom-js": "^0.19.13"
+  }
+}
+`;
 
-  STATIC SPA
+const layoutFile = `
+export tag Layout [
+  html lang='en' [
+    head [
+      title 'A-DOM'
+    ]
+    body [
+      yield
+    ]
+  ]
+]
+`;
 
-  adom.compile([
-    { input: '
-  ])
-  
- ***/
+const blogFile = `
+import 'layout.adom'
+
+Layout [
+  main [
+    h1 'Blog'
+    p '{{data.post}}'
+  ]
+]
+`;
+
+const indexFile = `
+import 'layout.adom'
+
+Layout [
+  main [
+    h1 'Home'
+    a href='/blog/post1' 'blog post 1'
+    a href='/blog/post2' 'blog post 2'
+  ]
+]
+`;
 
 for (let i = 0; i < process.argv.length; i++) {
   switch (process.argv[i]) {
-    case '-o':
-      config.out = process.argv[++i]
+    case 'create':
+      config.name = process.argv[++i];
       break
-    case '-p':
-      config.devPort = process.argv[++i]
+    case '--ssg':
+      config.ssg = true;
       break
-    case '-r':
-      let route = process.argv[++i];
-      let parts = route.split('=');
-      if (parts.length === 2) {
-        routes[parts[0]] = parts[1];
-      }
-      break
-    case '--dev':
-      config.dev = true;
-      break
-    case '--minify':
-      config.minify = true;
-      break
-    case '--new':
-      config.starter = true;
-      config.app = process.argv[++i];
+    case '--ssr':
+      config.ssr = true;
       break
     default:
-      config.file = process.argv[i];
       break
   }
 }
 
+if (
+  (config.ssr && config.ssg) ||
+  (!config.ssr && !config.ssg) ||
+  !config.name
+) {
+  console.log(help);
+} else {
+  const p = path.resolve(dir, config.name);
+
+  fs.mkdirSync(p);
+  fs.mkdirSync(path.join(p, 'public'));
+  fs.mkdirSync(path.join(p, 'src'));
+  fs.writeFileSync(path.join(p, 'src/index.adom'), indexFile);
+  fs.writeFileSync(path.join(p, 'src/blog.adom'), blogFile);
+  fs.writeFileSync(path.join(p, 'src/layout.adom'), layoutFile);
+
+  if (config.ssg) {
+    fs.writeFileSync(path.join(p, 'build.js'), ssgBuild);
+    fs.writeFileSync(path.join(p, 'package.json'), ssgPackageFile(config.name));
+  } else {
+    fs.writeFileSync(path.join(p, 'server.js'), ssrBuild);
+    fs.writeFileSync(path.join(p, 'package.json'), ssrPackageFile(config.name));
+  }
+}
